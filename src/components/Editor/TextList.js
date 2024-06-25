e71a51f034add7831129703449e20b660ae3848a
Novokshanov <Arxiv300800@gmail.com>
2024-06-25 17:05:39 +0500

    log

--- src/components/Editor/TextList.js
+++ src/components/Editor/TextList.js
@@ -14,6 +14,7 @@ const TextList = () => {
     const fetchTexts = async () => {
       try {
         const response = await axios.get(`${apiBaseUrl}/texts`);
+        console.log('Response data:', response.data);
         if (Array.isArray(response.data)) {
           setTexts(response.data);
         } else {
